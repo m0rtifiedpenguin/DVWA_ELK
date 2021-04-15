d649cc5186d31fedd48ee7f844ed6a33aa6b329b
Jacob Brumbaugh <80926733+m0rtifiedpenguin@users.noreply.github.com>
2021-04-15 15:13:05 -0400

    Update FileBeat.sh

--- FileBeat.sh
+++ FileBeat.sh
@@ -17,5 +17,5 @@ sudo dpkg -i filebeat-7.9.2-amd64.deb
 curl -L -O https://gist.githubusercontent.com/slape/5cc350109583af6cbe577bbcc0710c93/raw/eca603b72586fbe148c11f9c87bf96a63cb25760/Filebeat
 # Moving FileBeat
 mv Filebeat /etc/filebeat/filebeat.yml
-# Opening FileBeat with nano
+# Opening FileBeat config file with nano
 nano /etc/filebeat/filebeat.yml
